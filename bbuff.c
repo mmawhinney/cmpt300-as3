a94813f4c4fd7412faaf234d226aac38f7a73cf7
Matheson Mawhinney <matheson.mawhinney@gmail.com>
2016-03-16 18:59:26 -0700

    added random sleep times, correct factory IDs passed to factory thread, fixed buffer not using correct array index by changing it to use the value of full_buffer instead of a separate counter variable

--- bbuff.c
+++ bbuff.c
@@ -5,42 +5,44 @@
 #include <errno.h>
 #include "bbuff.h"
 
-sem_t fullBuffer;
-sem_t emptyBuffers;
+sem_t full_buffer;
+sem_t empty_buffers;
 sem_t mutex;
 void* buffer_data[BUFFER_SIZE];
 int buffer_count;
 
 void bbuff_init(void) {
-    sem_init(&fullBuffer, 0, 0);
-    sem_init(&emptyBuffers, 0, BUFFER_SIZE);
+    sem_init(&full_buffer, 0, 0);
+    sem_init(&empty_buffers, 0, BUFFER_SIZE);
     sem_init(&mutex, 0, 1);
     buffer_count = 0;
 }
 
 void bbuff_blocking_insert(void* item) {
-    // while(true) {
-        sem_wait(&emptyBuffers);
-        sem_wait(&mutex);
-        buffer_data[buffer_count++] = item;
-        sem_post(&mutex);
-        sem_post(&fullBuffer);
-    // }
+    int count = 0;
+    sem_wait(&empty_buffers);
+    sem_wait(&mutex);
+    sem_getvalue(&full_buffer, &count);
+    buffer_data[count] = item;
+    sem_post(&mutex);
+    sem_post(&full_buffer);
 }
 
 void* bbuff_blocking_extract(void) {
-    // while(true) {
-        sem_wait(&fullBuffer);
-        sem_wait(&mutex);
-        void* candy = &buffer_data[buffer_count--];
-        sem_post(&mutex);
-        sem_post(&emptyBuffers);
-        return candy;
-    // }
+    int count = 0;
+    sem_wait(&full_buffer);
+    sem_wait(&mutex);
+    sem_getvalue(&full_buffer, &count);
+    void* candy = buffer_data[count];
+    sem_post(&mutex);
+    sem_post(&empty_buffers);
+    return candy;
 }
 
 _Bool bbuff_is_empty(void) {
-    if(buffer_count == 0) {
+    int count = -1;
+    sem_getvalue(&full_buffer, &count);
+    if(count == 0) {
         return true;
     } else {
         return false;
--- candykids.c
+++ candykids.c
@@ -5,9 +5,10 @@
 #include <stdbool.h>
 #include <pthread.h>
 #include <errno.h>
-// #include <string.h>
+#include <sys/types.h>
 #include "bbuff.h"
 #include "stats.h"
+
 
 
 _Bool stop_thread = false;
@@ -20,7 +21,7 @@ struct candy_t {
 double current_time_in_ms(void) {
     struct timespec now;
     clock_gettime(CLOCK_REALTIME, &now);
-    return now.tv_sec * 1000.0 + now.tv_nsec/1000000.0;
+    return (now.tv_sec * 1000.0) + (now.tv_nsec/1000000.0);
 }
 
 void insert_candy(int factory_num) {
@@ -31,26 +32,29 @@ void insert_candy(int factory_num) {
 }
 
 void* factory_thread_func(void* arg) {
+    int* id = (int*)arg;
     while(!stop_thread) {
-        // TODO: pick random num between 0 and 3
-        // TODO: change 0 to the ID and 2s to the random number
-        write(STDOUT_FILENO, "\tFactory 0 ships candy & waits 2s\n", 34);
-        // TODO: change argument to id
-        insert_candy(0);
-        // TODO: change argument to random num from above
-        sleep(2);
+        // pick random num between 0 and 3
+        int sleep_time = rand() % 4;
+        printf("\tFactory %d ships candy & waits %ds\n", *id, sleep_time);
+        insert_candy(*id);
+        sleep(sleep_time);
     }
-    printf("Candy-factory 0 done\n");
+    printf("Candy-factory %d done\n", *id);
+    free(id);
     return NULL;
 }
 
 void* kid_thread_func(void* arg) {
     while(true) {
-        struct candy_t* candy = (struct candy_t*)bbuff_blocking_extract();
+        struct candy_t* candy = bbuff_blocking_extract();
         int factory_id = candy->factory_number;
         double timestamp = candy->time_stamp_in_ms;
         printf("Candy - Factory ID: %d, Timestamp: %f\n", factory_id, timestamp);
-        sleep(1);
+        free(candy);
+        // pick random num, either 0 or 1
+        int sleep_time = rand() % 2;
+        sleep(sleep_time);
     }
 }
 
@@ -72,6 +76,8 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
     
+    srand(time(NULL));
+    
     /*
     * initialize bounded buffer and stats modules
     */
@@ -84,8 +90,10 @@ int main(int argc, char* argv[]) {
     pthread_t factory_thread_ids[num_factories];
     
     for(int i = 0; i < num_factories; i++) {
+        int* id = malloc(sizeof(*id));
+        *id = i;
         pthread_t factory_id;
-        pthread_create(&factory_id, NULL, &factory_thread_func, NULL);
+        pthread_create(&factory_id, NULL, &factory_thread_func, id);
         if(errno < 0) {
             perror("Error creating factory thread\n");
         }
@@ -119,7 +127,6 @@ int main(int argc, char* argv[]) {
     stop_thread = true;
     for(int i = 0; i < num_factories; i++) {
         pthread_t id = factory_thread_ids[i];
-        pthread_cancel(id);
         pthread_join(id, NULL);
     }
     
